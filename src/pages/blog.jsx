211a3494f9b9cc4d42964b683685e17414781efc
Shrey Gupta <shreygupta97@gmail.com>
2021-01-18 23:48:03 -0800

    Removed fake blog posts

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -3,26 +3,31 @@ import React from 'react';
 import styled from 'styled-components';
 import { Layout } from '../components/Layout';
 
-const IndexWrapper = styled.main``;
-
 const PostWrapper = styled.div``;
 
 export default ({ data }) => {
+  const posts = data.allMdx.nodes;
+  if (posts.length === 0) {
+    return (
+      <Layout>
+        I just started up this blog so there's no posts yet. Check back soon!
+      </Layout>
+    )
+  }
+
   return (
     <Layout>
-      <IndexWrapper>
-        {data.allMdx.nodes.map(
-          ({ id, excerpt, frontmatter, fields }) => (
-            <PostWrapper key={id}>
-              <Link to={fields.slug}>
-                <h1>{frontmatter.title}</h1>
-                <p>{frontmatter.date}</p>
-                <p>{excerpt}</p>
-              </Link>
-            </PostWrapper>
-          )
-        )}
-      </IndexWrapper>
+      {posts.map(
+        ({ id, excerpt, frontmatter, fields }) => (
+          <PostWrapper key={id}>
+            <Link to={fields.slug}>
+              <h1>{frontmatter.title}</h1>
+              <p>{frontmatter.date}</p>
+              <p>{excerpt}</p>
+            </Link>
+          </PostWrapper>
+        )
+      )}
     </Layout>
   );
 };
---
+++ src/pages/projects.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Layout } from '../components/Layout';
+
+export default () => {
+  return (
+    <Layout>
+      I just started up this site so I haven't had a chance to upload projects yet. Check back soon for some cool stuff.
+    </Layout>
+  );
+}
