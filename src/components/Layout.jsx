875872b4c2013b49afc6b381107d6688e38ab584
Shrey Gupta <shreygupta97@gmail.com>
2021-01-19 23:46:45 -0800

    Fixed XHTML error in Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -25,8 +25,8 @@ export const Layout = ({ children }) => {
     <App>
       <Helmet>
         <title>Shrey Gupta</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       </Helmet>
       <Header siteTitle={title} />
       <Container>
